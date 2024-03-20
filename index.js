55b96a7f502628fb19f868cb817445c766f8ad84
Caesar <ignatiuscaesar16@gmail.com>
2024-03-21 00:41:39 +0300

    Inputs js code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+// Declare companyName as a constant and assign "Scuber" as its value.
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood using let and assign "Chelsea" as its value.
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo using let and assign "Susan Smith" as its value.
+let companyCeo = 'Susan Smith';
+
